5d24131953ffda627982e417b9b08adbcdd64a20
HoaAyWK <vycoi1511@gmail.com>
2021-08-22 23:30:39 -0700

    Add error handling for data fetching

--- src/Pages/AllMeetupPage.js
+++ src/Pages/AllMeetupPage.js
@@ -5,10 +5,13 @@ import MeetupCard from "../Components/Meetups/MeetupCard";
 
 function AllMeetupPage() {
   const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
   useEffect(() => {
     fetch(process.env.REACT_APP_API_ENDPOINT_MEETUPS)
       .then((response) => {
+          setIsLoading(true);
+          setIsError(false);
         return response.json();
       })
       .then((meetups) => {
@@ -22,10 +25,14 @@ function AllMeetupPage() {
         }
         setIsLoading(false);
         setData(meetupsData);
+      })
+      .catch(err=>{
+          setIsError(true);
       });
   }, []);
 
   if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Something went wrong...</div>
   return (
     <Row>
       {data.map((meetup, index) => {
