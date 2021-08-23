185e97f712b1902a1fe0fd134e81fbf87ae52145
HoaAyWK <vycoi1511@gmail.com>
2021-08-23 01:46:57 -0700

    Consuming context

--- src/Pages/FavoritesMeetupPage.js
+++ src/Pages/FavoritesMeetupPage.js
@@ -1,7 +1,36 @@
+import { Row, Col } from 'reactstrap';
+import { useContext } from "react";
+
+import MeetupCard from "../Components/Meetups/MeetupCard";
+import FavoritesContext from "../Stores/favorite-context";
+
 function FavoritesMeetupPage() {
-    return <div>
-        Favorties meetup page
-    </div>
+  let content;
+  const favoritesCotext = useContext(FavoritesContext);
+  if (favoritesCotext.totalFavorites === 0) {
+    content=<p>Nothing here</p>
+  } else {
+    content=<Row>
+        <h2>Your favorites</h2>      
+        {favoritesCotext.favorites.map((meetup, index) => {
+        return (
+          <Col sm="6" md="4" key={index}>
+            <MeetupCard 
+              key={meetup.id}
+              id={meetup.id}             
+              title={meetup.title}
+              description={meetup.description}
+              imgPath={meetup.imgPath}
+            />
+          </Col>
+        );
+      })}
+    </Row>
+  }
+
+  return (
+    <div>{content}</div>
+  );
 }
 
 export default FavoritesMeetupPage;
